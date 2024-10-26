baf9a73381cf76815daa8ff4686edf408b0ab51a
Ilan <mc_ilan@hotmail.com>
2024-10-26 16:17:17 -0300

    para mili

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from funciones_gestion import *
 #TEST ILAN CONTRA LUCCA
 #TEST SUBIDA FACU
 #TEST MILI
+#TEST
 graph = Graph("bolt://neo4j:12345678@localhost:7687")
 client = MongoClient('mongodb://localhost:27017/')
 db = client['hotel_db']

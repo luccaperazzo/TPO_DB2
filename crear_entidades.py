a666bb3c6098d1c4a4d844ef04ff894bca1f15ab
milicalle <milagroscallez89@gmail.com>
2024-10-26 16:32:25 -0300

    Cambios por los chicos

--- crear_entidades.py
+++ crear_entidades.py
@@ -27,7 +27,7 @@ def crear_hoteles():
         {"nombre": "Hotel Obelisco", "direccion": "Av. 9 de Julio 123", "telefono": "123456789", "email": "obelisco@example.com", "coordenadas": "-34.603684, -58.381559"},
         {"nombre": "Hotel Puerto Madero", "direccion": "Dique 1, Puerto Madero", "telefono": "987654321", "email": "puertomadero@example.com", "coordenadas": "-34.6083, -58.3629"},
         {"nombre": "Hotel Recoleta", "direccion": "Av. Alvear 1891", "telefono": "111222333", "email": "recoleta@example.com", "coordenadas": "-34.5883, -58.3974"},
-        {"nombre": "Hotel Palermo", "direccion": "Calle Serrano 1425", "telefono": "444555666", "email": "palermo@example.com", "coordenadas": "-34.5888, -58.4304"},
+        {"nombre": "Hotel Palermo", "dire6ccion": "Calle Serrano 1425", "telefono": "444555666", "email": "palermo@example.com", "coordenadas": "-34.5888, -58.4304"},
         {"nombre": "Hotel San Telmo", "direccion": "Calle Defensa 1047", "telefono": "777888999", "email": "santelmo@example.com", "coordenadas": "-34.6215, -58.3731"}
     ]
 

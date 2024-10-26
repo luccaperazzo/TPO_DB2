863cfcd347308bdcbd6e53bf31daa5642e77c927
Ilan <mc_ilan@hotmail.com>
2024-10-26 19:58:06 -0300

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from funciones_gestion import *
 #TEST ILAN CONTRA LUCCA
 #TEST SUBIDA FACU
 #TEST MILI
-
+# test
 
 graph = Graph("bolt://neo4j:12345678@localhost:7687")
 client = MongoClient('mongodb://localhost:27017/')
